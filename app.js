e43ac33c5d11d1a87e92121786d915d6e72452b8
Eric Stanulis <erstanulis@gmail.com>
2018-11-21 22:47:40 -0600

    Test commig

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/book', book);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+  const err = new Error('Not Found'); 
   err.status = 404;
   next(err);
 });
